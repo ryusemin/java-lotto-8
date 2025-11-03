53fd171ebdbc2e037545a58c52f064098b0b7c07
ryusemin <tpals3799@naver.com>
2025-11-03 16:27:43 +0900

    feat(WinningLotto): 당첨번호 관리 및 비교 기능 구현

---
+++ src/main/java/lotto/model/WinningLotto.java
@@ -0,0 +1,32 @@
+package lotto.model;
+
+public class WinningLotto {
+    private final Lotto winningNumbers;
+    private final int bonusNumber;
+
+    public WinningLotto(Lotto winningNumbers, int bonusNumber) {
+        validate(winningNumbers, bonusNumber);
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(Lotto winningNumbers, int bonusNumber) {
+        if (winningNumbers.getNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
+    }
+
+    // 당첨 번호 일치 개수 반환
+    public int countMatch(Lotto lotto) {
+        long match = lotto.getNumbers().stream()
+                .filter(winningNumbers.getNumbers()::contains)
+                .count();
+        return (int) match;
+    }
+
+    // 보너스 번호 일치 여부 반환
+    public boolean isBonusMatched(Lotto lotto) {
+        return lotto.getNumbers().contains(bonusNumber);
+    }
+}
+
