ad6907cb0baf052234881a62a54d1e48833b5b32
ryusemin <tpals3799@naver.com>
2025-11-03 23:25:25 +0900

    test(Rank): 전 등수(1~5등 및 꽝) 반환 로직 단위 테스트 추가

--- src/test/java/lotto/model/RankTest.java
+++ src/test/java/lotto/model/RankTest.java
@@ -18,10 +18,30 @@ class RankTest {
         assertThat(Rank.valueOf(5, true)).isEqualTo(Rank.SECOND);
     }
 
+    @DisplayName("5개 번호 일치 시 3등을 반환한다.")
+    @Test
+    void 랭크_3등() {
+        assertThat(Rank.valueOf(5, false)).isEqualTo(Rank.THIRD);
+    }
+
+    @DisplayName("4개 번호 일치 시 4등을 반환한다.")
+    @Test
+    void 랭크_4등() {
+        assertThat(Rank.valueOf(4, false)).isEqualTo(Rank.FOURTH);
+    }
+
     @DisplayName("3개 번호 일치 시 5등을 반환한다.")
     @Test
     void 랭크_5등() {
         assertThat(Rank.valueOf(3, false)).isEqualTo(Rank.FIFTH);
+    }
+
+    @DisplayName("2개 이하 일치 시 등수가 없다.")
+    @Test
+    void 랭크_꽝() {
+        assertThat(Rank.valueOf(2, false)).isEqualTo(Rank.NONE);
+        assertThat(Rank.valueOf(1, false)).isEqualTo(Rank.NONE);
+        assertThat(Rank.valueOf(0, false)).isEqualTo(Rank.NONE);
     }
 }
 
